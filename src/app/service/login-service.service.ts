b819a01fa035efcd0d7faa1eeb6aee3c3da35def
ThanhNt <20212107@eaut.edu.vn>
2024-07-22 11:33:27 +0700

    fix: login with google

--- src/app/service/login-service.service.ts
+++ src/app/service/login-service.service.ts
@@ -1,9 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable, Injector } from '@angular/core';
 import { environment } from '../../environments/environment';
-import { SocialAuthService, SocialUser } from '@abacritt/angularx-social-login';
-import { GoogleLoginProvider } from '@abacritt/angularx-social-login';
-import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -11,22 +8,15 @@ import { Subject } from 'rxjs';
 export class LoginServiceService {
   endPoint: any;
 
-  private authChangeSub = new Subject<boolean>();
-  private extAuthChangeSub = new Subject<SocialUser>();
-  public authChanged = this.authChangeSub.asObservable();
-  public extAuthChanged = this.extAuthChangeSub.asObservable();
-
   constructor(
     private _http: HttpClient,
-    _injector: Injector,
-    private externalAuthService: SocialAuthService
+    _injector: Injector
   ) {
     this.endPoint = `${environment.apiDomain.devEndpoint}/${environment.apiDomain.gateway}`;
   }
 
   login(model: any): Promise<any> {
     const apiUrl = `${this.endPoint}/auth/login`;
-    this.extAuthChangeSub.next(model);
     return this._http
       .post<any>(apiUrl, model)
       .toPromise()
@@ -34,11 +24,4 @@ export class LoginServiceService {
         return Promise.reject(error.message || error);
       });
   }
-
-  public signInWithGoogle = () => {
-    this.externalAuthService.signIn(GoogleLoginProvider.PROVIDER_ID);
-  };
-  public signOutExternal = () => {
-    this.externalAuthService.signOut();
-  };
 }
